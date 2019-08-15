c9a53de6faff7d3a14cf9b624513cb25f4a9b6b4
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-15 09:05:52 +0700

    Add useEffect function

--- react-hook/react-hook.js
+++ react-hook/react-hook.js
@@ -12,6 +12,17 @@ const React = (() => {
     return [state, setState]
   }
 
+  const useEffect = (cb, depArr) => {
+    const oldDeps = hooks[idx]
+    let hasChanged = true
+    if (oldDeps) {
+      hasChanged = depArr.some((dep, index) => !Object.is(dep, oldDeps[index]))
+    }
+    if (hasChanged) cb()
+    hooks[idx] = depArr
+    idx++
+  }
+
   const render = Component => {
     idx = 0
     const c = Component()
@@ -19,15 +30,16 @@ const React = (() => {
     return c
   }
 
-  return { useState, render }
+  return { useState, useEffect, render }
 })()
 
 const MyComponent = () => {
   const [count, setCount] = React.useState(1)
   const [text, setText] = React.useState('default name')
+  React.useEffect(() => { console.log('changed') }, [text, count])
 
   return {
-    render: () => console.log({count, text}),
+    render: () => console.log({ count, text }),
     click: () => setCount(count + 1),
     type: word => setText(word),
   }
