be634cb01bae7e482059bbc0047f05eb278ac9d5
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-18 22:11:20 +0700

    Option1 Provider Consumer

--- react-context/src/App.js
+++ react-context/src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css';;
+
+const MyContext = React.createContext();
+
+class Button extends Component {
+  render() {
+    return (
+      <MyContext.Consumer>
+        {(context) => (
+            <React.Fragment>
+              <p>Name: {context.user.name}</p>
+              <p>Age: {context.user.age}</p>
+              <button onClick={context.updateAgeUser}>Click here</button>
+            </React.Fragment>
+          )}
+      </MyContext.Consumer>
+    )
+  }
+}
+
+const MyWrapper = props => (
+  <div>
+    <Button></Button>
+  </div>
+)
+
+class MyProvider extends Component {
+  state = {
+    name: 'Atichat',
+    age: 23,
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        user: this.state,
+        updateAgeUser: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyProvider>
+        <header className="App-header" />
+        <div>
+          <p> Some Compoennt 1
+          <MyWrapper />
+            <p> Some Compoennt 2 
+              <MyWrapper />
+            </p>
+          </p>
+        </div>
+        <div>
+          <p> Footer Component 
+          <MyWrapper />
+          </p>
+        </div>
+      </MyProvider>
     </div>
   );
 }
