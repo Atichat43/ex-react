9c3796f815f41f3f4cc78942b4e54d493072cea7
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-24 15:35:12 +0700

    Init app

---
+++ react-higher-order-components/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Higher-order component </p>
+      </header>
+      {/* our component here */}
+    </div>
+  );
+}
+
+export default App;
