399ea79d2831f2f0130cc1d77d4a198a9034d922
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-24 15:47:03 +0700

    Fetch contacts from randomuser

--- react-higher-order-components/src/App.js
+++ react-higher-order-components/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> Higher-order component </p>
-      </header>
-      {/* our component here */}
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+  }
+
+  componentDidMount() {
+    fetch('https://api.randomuser.me/?results=50')
+      .then(res => res.json())
+      .then(res => res.results.map(user => ({
+        name: `${user.name.first} ${user.name.last}`,
+        gender: user.gender,
+        email: user.email,
+        phone: user.phone,
+      })))
+      .then(res => this.setState(() => ({ contacts: res })))
+  }
+
+  render() {
+    const { contacts } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Higher-order component</p>
+          <p>Number of contacts: {contacts && contacts.length}</p>
+        </header>
+        {/* our component here */}
+      </div>
+    )
+  }
 }
 
 export default App;
